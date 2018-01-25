103ea5304d7baf2b7497f008fafb1b42f298ca33
Francisco Cháves <pachopepe@gmail.com>
2018-01-25 00:17:21 -0500

    QuickTheories

--- src/test/java/edu/eci/pdsw/test/TransformationsTest.java
+++ src/test/java/edu/eci/pdsw/test/TransformationsTest.java
@@ -8,14 +8,20 @@ package edu.eci.pdsw.test;
 import eci.pdsw.draw.controller.Controller;
 import eci.pdsw.draw.model.ElementType;
 import eci.pdsw.draw.model.Shape;
+import eci.pdsw.util.Pair;
 import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import org.quicktheories.core.Gen;
+import static org.quicktheories.QuickTheory.qt;
+import static org.quicktheories.generators.Generate.*;
+import static org.quicktheories.generators.SourceDSL.*;
+
 /**
  *
- * @author hcadavid
+ * @author hcadavid, fchaves
  */
 public class TransformationsTest {
     
@@ -27,33 +33,54 @@ public class TransformationsTest {
     }
 
     @Test
-    public void duplicateTest(){
-        Controller guictrl=new Controller();        
-        guictrl.setSelectedElementType(ElementType.Line);
-        guictrl.addShapeFromScreenPoints(new java.awt.Point(10,10),new java.awt.Point(100, 100));
-        guictrl.addShapeFromScreenPoints(new java.awt.Point(20,20),new java.awt.Point(120, 120));
-        
-        guictrl.duplicateShapes();
-        
-        
-        assertEquals("duplicateShapes in not duplicating the amount of shapes",guictrl.getShapes().size(), 4);
-        
-        List<Shape> shapes=guictrl.getShapes();
-        
-        
-        for (Shape si:shapes){
-            int refcount=0;
-            for (Shape sj:shapes){
-                if (si==sj) refcount++;
-            }
-            if (refcount>1){
-                fail("duplicateShapes method is generating duplicated references");                        
-            }
-        }
-        
+    public void duplicateTestSize(){
+        qt().forAll(linesController())
+            .check((controller) -> {
+                    int n = controller.getShapes().size();
+                    controller.duplicateShapes();
+                    int m = controller.getShapes().size();
+                    
+                    return m == 2 * n;
+                });
     }
 
 
+    @Test
+    public void duplicateTestNotDuplicatedReferences() {
+        qt().forAll(linesController())
+            .check((controller) -> {
+                    controller.duplicateShapes();
+                    List<Shape> shapes = controller.getShapes();
 
+                    return shapes.stream()
+                        .allMatch((si) -> 
+                                  shapes.stream().filter((sj) -> si == sj ).count() == 1);
+                });
+    }
+
+
+    private Gen<Controller> linesController() {
+        return listsLineAsPoints().map( (ls) -> {
+                    Controller guictrl=new Controller();        
+                    guictrl.setSelectedElementType(ElementType.Line);
+                    for(Pair<java.awt.Point,java.awt.Point> p : ls) {
+                        guictrl.addShapeFromScreenPoints(p.getFirst(),p.getSecond());                        
+                    }
+                    return guictrl;
+                });
+    }
+
+    private Gen<List<Pair<java.awt.Point,java.awt.Point>>> listsLineAsPoints() {        
+        return lists().of(pairOfPoints()).ofSizeBetween(0,20);
+    }
+
+    private Gen<Pair<java.awt.Point,java.awt.Point>> pairOfPoints() {
+        return points().zip(points(),(p1,p2) -> new Pair(p1,p2));
+    }
+
+    private Gen<java.awt.Point> points() {
+        return integers().allPositive()
+            .zip(integers().allPositive(), (x,y) -> new java.awt.Point(x,y));
+    }
     
 }
